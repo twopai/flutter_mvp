a77f3e49d49d1813c0d1a11965af530555c3518c
李迅 <292721865@qq.com>
2019-10-17 16:10:07 +0800

    twopai mvp总结

--- lib/page/runshoppage.dart
+++ lib/page/runshoppage.dart
@@ -64,7 +64,6 @@ class _RunShopPageState extends BaseState<RunShopPage, RunShopPresenter> impleme
     print('Device height 0-->dp:${size2.height}'); //Device height
     //设置适配尺寸 (填入设计稿中设备的屏幕尺寸) 假如设计稿是按iPhone6的尺寸设计的(iPhone6 750*1334)
     ScreenUtil.instance = ScreenUtil(width: size2.width, height: size2.height)..init(context);
-
     print('Device width px:${ScreenUtil.screenWidth}'); //Device width
     print('Device height px:${ScreenUtil.screenHeight}'); //Device height
     print(
